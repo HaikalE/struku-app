a0050bdf8c6ceea04283c8411a99c66f0d8aa2f8
Muhammad Haikal Rahman <89823572+HaikalE@users.noreply.github.com>
2025-04-02 12:03:13 +0800

    Add Hilt Application class

---
+++ app/src/main/java/com/example/struku/StrukuApplication.kt
@@ -0,0 +1,10 @@
+package com.example.struku
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Main application class with Hilt dependency injection
+ */
+@HiltAndroidApp
+class StrukuApplication : Application()

3a6d776b70cf37efaf83458a618707fc14ad6074
Muhammad Haikal Rahman <89823572+HaikalE@users.noreply.github.com>
2025-04-03 09:09:28 +0800

    Fix Gradle dependency issues: Update repositories configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,12 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") } // Tambahkan ini untuk MPAndroidChart
+        maven { url = uri("https://jitpack.io") } // For MPAndroidChart
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") } // For Compose
+        // For TensorFlow
+        maven { url = uri("https://storage.googleapis.com/download.tensorflow.org/repos/tensorflow") }
+        // For OpenCV
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
     }
 }
 

4962a62bb20a837dd0dedd9bf0623a49080bb565
Muhammad Haikal Rahman <89823572+HaikalE@users.noreply.github.com>
2025-04-03 17:38:00 +0800

    Update DatabaseModule to provide BudgetDao and CategoryDao

--- app/src/main/java/com/example/struku/di/DatabaseModule.kt
+++ app/src/main/java/com/example/struku/di/DatabaseModule.kt
@@ -3,6 +3,8 @@ package com.example.struku.di
 import android.content.Context
 import androidx.room.Room
 import com.example.struku.data.local.StrukuDatabase
+import com.example.struku.data.local.dao.BudgetDao
+import com.example.struku.data.local.dao.CategoryDao
 import com.example.struku.data.local.dao.ReceiptDao
 import dagger.Module
 import dagger.Provides
@@ -38,5 +40,23 @@ object DatabaseModule {
     @Singleton
     fun provideReceiptDao(database: StrukuDatabase): ReceiptDao {
         return database.receiptDao()
+    }
+    
+    /**
+     * Provide category DAO
+     */
+    @Provides
+    @Singleton
+    fun provideCategoryDao(database: StrukuDatabase): CategoryDao {
+        return database.categoryDao()
+    }
+    
+    /**
+     * Provide budget DAO
+     */
+    @Provides
+    @Singleton
+    fun provideBudgetDao(database: StrukuDatabase): BudgetDao {
+        return database.budgetDao()
     }
 }
